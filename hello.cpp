67c440c06937143d3e88050c4379199ab14ef64e
thormas <thormas1024@googlemail.com>
2012-01-02 18:58:47 +0100

    1.52/1.62 a new quickfix

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,14 @@
-// version 1.51 quickfix
+// version 1.52 (changed after 1.6 committed, so actually 1.62) quickfix
 
 #include <stdio.h>
 #include <math.h>
 
 void main() {
 	printf("hello, asshole");	// fixing a little error, but not changing too much
+	newFun();
 	printf("Sinus(pi)=", sin(PI));
+}
+
+void newFun() {
+	x+= 7;
 }
